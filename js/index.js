83e5729e4fc3928b3fd5d613bafa7d46f3bfe4c6
blazejjestem <blazej0wasielewski@gmail.com>
2022-05-11 00:19:17 +0200

    Very raw mobile styles done.

---
+++ js/index.js
@@ -0,0 +1,7 @@
+const toggleButton = document.querySelector(".toggle-button");
+
+const navLinks = document.querySelector(".nav-links");
+
+toggleButton.addEventListener("click", () => {
+  navLinks.classList.toggle("nav-links__show");
+});
